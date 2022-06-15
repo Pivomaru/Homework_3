d7c76298fc8cef2f6b9cbfc79630b1dd6455088d
Pivomaru <spardasenpai@gmail.com>
2022-06-15 15:16:06 +0300

    Zadacha3

--- Program.cs
+++ Program.cs
@@ -8,7 +8,9 @@
 
 /*  Напишите программу, которая принимает на
 вход координаты двух точек и находит расстояние между
-ними в 3D пространстве.*/ 
+ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53 */ 
 
  /* Console.WriteLine("Введите x1");
 int x1 = int.Parse(Console.ReadLine());
@@ -31,3 +33,21 @@ int z2 =  int.Parse(Console.ReadLine());
 double result = Math.Sqrt (Math.Pow(x1 - x2,2) + Math.Pow(y1 - y2,2) + Math.Pow(z1 - z2,2));
 
 Console.WriteLine(result); */ 
+
+/////////////////////////////////////////
+
+/* Напишите программу, которая принимает на
+вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 4, 9.
+5 -> 1, 8, 27, 64, 125  */ 
+
+ /* Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine());
+
+int Count = 1;
+
+while (Count <= N)
+{
+    Console.Write(Math.Pow(Count,3) + " ");
+    Count++ ;
+} */ 
