d944b8582ade407b039e95ad026b7562f06c7d97
Pivomaru <spardasenpai@gmail.com>
2022-06-15 15:09:58 +0300

    Zadacha2

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿ /* Напишите программу, которая принимает на
+вход пятизначное число и проверяет, является ли оно
+палиндромом.
+14212 -> нет
+23432 -> да
+12821 -> дa */
+
+
+/*  Напишите программу, которая принимает на
+вход координаты двух точек и находит расстояние между
+ними в 3D пространстве.*/ 
+
+ /* Console.WriteLine("Введите x1");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите y1");
+int y1 =  int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите z1");
+int z1 =  int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите x2");
+int x2 =  int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите y2");
+int y2 =  int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите z2");
+int z2 =  int.Parse(Console.ReadLine());
+
+double result = Math.Sqrt (Math.Pow(x1 - x2,2) + Math.Pow(y1 - y2,2) + Math.Pow(z1 - z2,2));
+
+Console.WriteLine(result); */ 
